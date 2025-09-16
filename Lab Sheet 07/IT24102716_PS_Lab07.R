1d54b3429dd6381e2bbc267642caa31bd0e9e740
IT24102716 <it24102716@my.sliit.lk>
2025-09-16 16:20:35 +0530

    Lab 07 uploaded

---
+++ Lab Sheet 07/IT24102716_PS_Lab07.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\HIRU\\OneDrive\\Desktop\\IT24102716")
+getwd()
+
+#Exercise 01
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+#Exercise 02
+pexp(2, rate=1/3)
+
+#Exercise 03
+#part i
+1 - pnorm(130, mean=100, sd=15)
+
+#Part ii
+qnorm(0.95, mean=100, sd=15)
+
